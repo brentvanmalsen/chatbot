1be74ac8263d376989f168341d87cad6a309119f
Brent van Malsen <i507960@student.fontys.nl>
2024-03-05 23:33:40 +0100

    Opnieuw begonnen door een error

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,3 @@
-import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -9,38 +8,8 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-  final ChatUser _currentUser =
-      ChatUser(id: '1', firstName: 'Brent', lastName: 'van Malsen');
-
-  final ChatUser _gptChatUser =
-      ChatUser(id: '2', firstName: 'Chat', lastName: 'GPT');
-
-  List<ChatMessage> _messages = <ChatMessage>[];
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(0, 166, 126, 1), // Color.fromRGB0
-        title: const Text(
-          'GPT Chat',
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),
-      body: DashChat(
-          currentUser: _currentUser, 
-          onSend: (ChatMessage m) {
-            getChatResponse(m);
-          }, 
-          messages: _messages),
-    );
-  }
-
-  Future<void> getChatResponse(ChatMessage m) async {
-    setState(() {
-      
-    });
+    return Scaffold();
   }
 }
